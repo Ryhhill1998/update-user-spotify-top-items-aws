da076e561a1deedb642891dcdc60c45915bbd4bb
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-11 11:25:43 +0100

    added httpx as deploy test

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,8 @@
 import json
 import os
-from dataclasses import dataclass
+from enum import Enum
+
+import httpx
 import mysql.connector
 
 conn = mysql.connector.connect(
@@ -10,9 +12,36 @@ conn = mysql.connector.connect(
     password=os.environ.get("DB_PASS")
 )
 
-cursor = conn.cursor()
-cursor.execute("SELECT VERSION();")
-version = cursor.fetchone()
+
+class ItemType(Enum):
+    ARTISTS = "artists"
+    TRACKS = "tracks"
+
+
+class TimeRange(Enum):
+    SHORT = "short_term"
+    MEDIUM = "medium_term"
+    LONG = "long_term"
+
+
+# async def get_spotify_top_items(client: , access_token: str, item_type: ItemType, time_range: TimeRange):
+#     try:
+#         params = {"time_range": time_range, "limit": 50}
+#         url = f"{self.base_url}/me/top/{item_type.value}?" + urllib.parse.urlencode(params)
+#
+#         data = await self.endpoint_requester.get(url=url, headers={"Authorization": f"Bearer {access_token}"})
+#
+#         top_items = [self._create_item(data=entry, item_type=item_type) for entry in data["items"]]
+#
+#         return top_items
+#     except EndpointRequesterUnauthorisedException as e:
+#         error_message = "Invalid Spotify API access token"
+#         logger.error(f"{error_message} - {e}")
+#         raise SpotifyDataServiceUnauthorisedException(error_message)
+#     except EndpointRequesterException as e:
+#         error_message = "Failed to make request to Spotify API"
+#         logger.error(f"{error_message} - {e}")
+#         raise SpotifyDataServiceException(error_message)
 
 
 def lambda_handler(event, context):
@@ -23,8 +52,4 @@ def lambda_handler(event, context):
         message = json.loads(record["body"])
         print(f"{message = }")
 
-    # TODO implement
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"version": version})
-    }
+    client = httpx.AsyncClient()
