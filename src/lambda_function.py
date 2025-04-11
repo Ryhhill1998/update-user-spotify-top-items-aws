1d448c94807d446275b89213f045b13bacf6f491
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-11 14:49:27 +0100

    finished code to allow db updates and retrieving top items

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,25 +2,84 @@ import json
 import os
 import httpx
 import mysql.connector
+import asyncio
+from dataclasses import dataclass
+from datetime import datetime, timezone
 
-conn = mysql.connector.connect(
-    host=os.environ.get("DB_HOST"),
-    database=os.environ.get("DB_NAME"),
-    user=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASS")
-)
+from src.db_service import DBService
+from src.spotify_service import SpotifyService, TimeRange, ItemType
 
 
-def get_user_data_from_records(records: list[dict]):
-    pass
+@dataclass
+class User:
+    id: str
+    refresh_token: str
+
+
+def get_user_data_from_event(event: dict):
+    record = event["Records"][0]
+    data = json.loads(record["body"])
+    user = User(id=data["user_id"], refresh_token=data["refresh_token"])
+    return user
+
+
+async def get_user_top_items_data_for_all_time_ranges(spotify_service: SpotifyService, access_token: str):
+    tasks = []
+
+    for time_range in TimeRange:
+        for item_type in ItemType:
+            get_top_items_task = spotify_service.get_top_items(
+                base_url=f"{os.environ.get("SPOTIFY_DATA_BASE_URL")}/me/top",
+                access_token=access_token,
+                item_type=item_type.value,
+                time_range=time_range.value
+            )
+            tasks.append(get_top_items_task)
+
+    all_top_items = await asyncio.gather(*tasks)
+    return all_top_items
+
+
+async def main(event):
+    # Create required classes
+    client = httpx.AsyncClient()
+    spotify_service = SpotifyService(client)
+
+    connection = mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASS")
+    )
+    db_service = DBService(connection)
+
+    # 1. Get user_id and refresh_token from event records
+    user = get_user_data_from_event(event)
+
+    # 2. Refresh user's access_token to Spotify API using refresh_token.
+    tokens = await spotify_service.refresh_tokens(
+        url=f"{os.environ.get("SPOTIFY_AUTH_BASE_URL")}/api/token",
+        client_id=os.environ.get("SPOTIFY_CLIENT_ID"),
+        client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET"),
+        refresh_token=user.refresh_token
+    )
+
+    # 3. If new refresh_token is returned, update it in the DB.
+    if tokens.refresh_token is not None:
+        db_service.update_refresh_token(user_id=user.id, refresh_token=tokens.refresh_token)
+
+    # 4. Get user's top artists and tracks from Spotify API for all time ranges.
+    all_top_items_data = await get_user_top_items_data_for_all_time_ranges(
+        spotify_service=spotify_service,
+        access_token=tokens.access_token
+    )
+
+    # 5. Store all top items in DB.
+    collected_date = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+
+    for top_items_data in all_top_items_data:
+        db_service.store_top_items(user_id=user.id, top_items_data=top_items_data, collected_date=collected_date)
 
 
 def lambda_handler(event, context):
-    records = event["Records"]
-
-    for record in records:
-        print(f"{record = }")
-        message = json.loads(record["body"])
-        print(f"{message = }")
-
-    client = httpx.AsyncClient()
+    asyncio.run(main(event))
--- src/spotify_service.py
+++ src/spotify_service.py
@@ -18,16 +18,27 @@ class TimeRange(Enum):
 @dataclass
 class Tokens:
     access_token: str
-    refresh_token: str
+    refresh_token: str | None = None
+
+
+@dataclass
+class TopItem:
+    id: str
+    position: int
+
+
+@dataclass
+class TopItemsData:
+    top_items: list[TopItem]
+    item_type: str
+    time_range: str
 
 
 class SpotifyService:
-    def __init__(self, client: httpx.AsyncClient, base_url: str):
+    def __init__(self, client: httpx.AsyncClient):
         self.client = client
-        self.base_url = base_url
 
-    async def refresh_tokens(self, client_id: str, client_secret: str, refresh_token: str) -> Tokens:
-        url = f"{self.base_url}/api/token"
+    async def refresh_tokens(self, url: str, client_id: str, client_secret: str, refresh_token: str) -> Tokens:
         headers = {
             "Authorization": f"Basic {base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()}",
             "Content-Type": "application/x-www-form-urlencoded"
@@ -38,38 +49,24 @@ class SpotifyService:
         res.raise_for_status()
         token_data = res.json()
 
-        access_token = token_data.get("access_token")
-        refresh_token = token_data.get("refresh_token", refresh_token)
-        refreshed_tokens = Tokens(access_token=access_token, refresh_token=refresh_token)
-
+        refreshed_tokens = Tokens(**token_data)
         return refreshed_tokens
 
-
-    async def _get_top_items(self, access_token: str, item_type: ItemType, time_range: TimeRange) -> list[dict]:
-        url = f"{self.base_url}/me/top/{item_type.value}"
+    async def get_top_items(
+            self,
+            base_url: str,
+            access_token: str,
+            item_type: str,
+            time_range: str
+    ) -> TopItemsData:
+        url = f"{base_url}/{item_type.value}"
         params = {"time_range": time_range, "limit": 50}
 
         res = await self.client.get(url=url, params=params, headers={"Authorization": f"Bearer {access_token}"})
         res.raise_for_status()
         data = res.json()
 
-        top_items = data["items"]
+        top_items_data = data["items"]
+        top_items = [TopItem(id=entry["id"], position=index + 1) for index, entry in enumerate(top_items_data)]
+        top_items = TopItemsData(top_items=top_items, item_type=item_type, time_range=time_range)
         return top_items
-
-    async def get_top_artists_ids(self, access_token: str, time_range: TimeRange) -> list[str]:
-        top_artists_data = await self._get_top_items(
-            access_token=access_token,
-            item_type=ItemType.TRACKS,
-            time_range=time_range
-        )
-        top_artists_ids = [entry["id"] for entry in top_artists_data]
-        return top_artists_ids
-
-    async def get_top_tracks_ids(self, access_token: str, time_range: TimeRange) -> list[str]:
-        top_tracks_data = await self._get_top_items(
-            access_token=access_token,
-            item_type=ItemType.TRACKS,
-            time_range=time_range
-        )
-        top_tracks_ids = [entry["id"] for entry in top_tracks_data]
-        return top_tracks_ids
