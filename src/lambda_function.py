94ab54e036d857b57fa6e4d3c8347e4995266588
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-16 12:30:09 +0100

    updated lambda function to use updated spotify service

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,14 +2,32 @@ import json
 import os
 from dataclasses import asdict
 
-import boto3
 import httpx
 import asyncio
-
+import boto3
 from botocore.client import BaseClient
 
-from spotify_service import SpotifyService, TimeRange, ItemType, TopItemsData
+
+from spotify_service import SpotifyService, TopItemsData
 from src.models import User, Settings
+
+
+def get_settings() -> Settings:
+    spotify_client_id = os.environ["SPOTIFY_CLIENT_ID"]
+    spotify_client_secret = os.environ["SPOTIFY_CLIENT_SECRET"]
+    spotify_auth_base_url = os.environ["SPOTIFY_AUTH_BASE_URL"]
+    spotify_data_base_url = os.environ["SPOTIFY_DATA_BASE_URL"]
+    queue_url = os.environ["QUEUE_URL"]
+
+    settings = Settings(
+        spotify_client_id=spotify_client_id,
+        spotify_client_secret=spotify_client_secret,
+        spotify_auth_base_url=spotify_auth_base_url,
+        spotify_data_base_url=spotify_data_base_url,
+        queue_url=queue_url
+    )
+
+    return settings
 
 
 def get_user_data_from_event(event: dict) -> User:
@@ -17,27 +35,6 @@ def get_user_data_from_event(event: dict) -> User:
     data = json.loads(record["body"])
     user = User(id=data["user_id"], refresh_token=data["refresh_token"])
     return user
-
-
-async def fetch_all_top_items(
-        spotify_service: SpotifyService,
-        spotify_data_base_url: str,
-        access_token: str
-) -> list[TopItemsData]:
-    tasks = []
-
-    for time_range in TimeRange:
-        for item_type in ItemType:
-            get_top_items_task = spotify_service.get_top_items(
-                base_url=f"{spotify_data_base_url}/me/top",
-                access_token=access_token,
-                item_type=item_type,
-                time_range=time_range
-            )
-            tasks.append(get_top_items_task)
-
-    all_top_items = await asyncio.gather(*tasks)
-    return all_top_items
 
 
 def add_user_spotify_data_to_queue(
@@ -57,57 +54,30 @@ def add_user_spotify_data_to_queue(
     print(f"{res = }")
 
 
-async def get_user_spotify_data(settings: Settings, user: User):
-    client = httpx.AsyncClient()
-
-    try:
-        spotify_service = SpotifyService(client)
-
-        # 1. Refresh access token
-        tokens = await spotify_service.refresh_tokens(
-            url=f"{settings.spotify_auth_base_url}/api/token",
-            client_id=settings.spotify_client_id,
-            client_secret=settings.spotify_client_secret,
-            refresh_token=user.refresh_token
-        )
-
-        # 2. Fetch top items
-        all_top_items_data = await fetch_all_top_items(
-            spotify_service=spotify_service,
-            spotify_data_base_url=f"{settings.spotify_data_base_url}/me/top",
-            access_token=tokens.access_token
-        )
-        return all_top_items_data, tokens.refresh_token
-    except Exception as e:
-        print(f"Something went wrong - {e}")
-    finally:
-        await client.aclose()
-
-
-def lambda_handler(event, context):
-    spotify_client_id = os.environ["SPOTIFY_CLIENT_ID"]
-    spotify_client_secret = os.environ["SPOTIFY_CLIENT_SECRET"]
-    spotify_auth_base_url = os.environ["SPOTIFY_AUTH_BASE_URL"]
-    spotify_data_base_url = os.environ["SPOTIFY_DATA_BASE_URL"]
-    queue_url = os.environ["QUEUE_URL"]
-
-    settings = Settings(
-        spotify_client_id=spotify_client_id,
-        spotify_client_secret=spotify_client_secret,
-        spotify_auth_base_url=spotify_auth_base_url,
-        spotify_data_base_url=spotify_data_base_url,
-        queue_url=queue_url
-    )
-
+async def main(event):
+    settings = get_settings()
     user = get_user_data_from_event(event)
 
-    all_top_items_data, refresh_token = asyncio.run(get_user_spotify_data(settings=settings, user=user))
+    with httpx.AsyncClient() as client:
+        spotify_service = SpotifyService(
+            client=client,
+            client_id=settings.spotify_client_id,
+            client_secret=settings.spotify_client_secret,
+            auth_url=settings.spotify_auth_base_url,
+            data_base_url=settings.spotify_data_base_url
+        )
+
+        user_spotify_data = await spotify_service.get_user_spotify_data(user.refresh_token)
 
     sqs = boto3.client("sqs")
     add_user_spotify_data_to_queue(
         sqs=sqs,
         queue_url=settings.queue_url,
         user_id=user.id,
-        refresh_token=refresh_token,
-        all_top_items_data=all_top_items_data
+        refresh_token=user_spotify_data.refresh_token,
+        all_top_items_data=user_spotify_data.data
     )
+
+
+def lambda_handler(event, context):
+    asyncio.run(main(event))
